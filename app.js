61b16ea9491a17644217150a7a914f3f6f0e7f41
Andrey Zinoviev <dronis_dronis@mail.ru>
2020-12-03 18:33:15 +0300

    cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect(MONGO_URL, {
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://t29.students.nomoreparties.co/',
 }));
 
 app.use(requestLogger);
